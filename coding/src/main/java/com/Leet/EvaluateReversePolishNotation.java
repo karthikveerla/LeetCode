b741da35ac3c531d968e64351fee8c23d61fd937
Karthik Veerla <karthikveerla28@gmail.com>
2025-12-30 15:09:54 -0500

    Leetcode 150 - Evaluate Reverse Polish Notation

---
+++ coding/src/main/java/com/Leet/EvaluateReversePolishNotation.java
@@ -0,0 +1,40 @@
+package com.Leet;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+
+    private static int findValue(String[] s){
+        Stack<Integer> st = new Stack<>();
+        for(String ss:s){
+            if(isSymbol(ss)){
+                int a = st.pop();
+                int b = st.pop();
+                st.push(Evaluate(ss,a,b));
+            }
+            else{
+                st.push(Integer.parseInt(ss));
+            }
+        }
+        return st.pop();
+    }
+
+    private static int Evaluate(String symbol, int a, int b){
+        switch (symbol) {
+            case "+":  return a+b; 
+            case "-":  return a-b;
+            case "*":  return a*b;
+            case "/":  return a/b;
+            default: throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+
+    private static boolean isSymbol(String p){
+        return p.equals("+")|| p.equals("-")||p.equals("*")||p.equals("/");
+    }
+
+    public static void main(String[] args) {
+        String[] s = {"2","1","+","3","*"};
+        System.out.println(findValue(s));
+    }
+}
