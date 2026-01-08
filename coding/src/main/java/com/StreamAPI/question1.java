feeb7da211df1a33ae6a71c262c4f75703bdf00d
Karthik Veerla <karthikveerla28@gmail.com>
2026-01-08 16:44:55 -0500

    Stream filtering,sorting

---
+++ coding/src/main/java/com/StreamAPI/question1.java
@@ -0,0 +1,15 @@
+package com.StreamAPI;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class question1 {
+    public static void main(String[] args) {
+        List<String> s = Arrays.asList("alice","bob","chris","david","erikson","fernando","greg","heather","irfan");
+        System.out.println(s.stream()
+        .filter(p->p.contains("a"))
+        .sorted(Comparator.comparing(String::length))
+        .toList());
+    }
+}
