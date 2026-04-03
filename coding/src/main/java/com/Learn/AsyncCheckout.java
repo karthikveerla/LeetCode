768a8fa7c3dd7553aecb8c255a464fbb4f9e51a8
Karthik Veerla <karthikveerla28@gmail.com>
2026-04-03 00:16:35 -0400

    updated async

--- coding/src/main/java/com/Learn/AsyncCheckout.java
+++ coding/src/main/java/com/Learn/AsyncCheckout.java
@@ -5,20 +5,30 @@ import java.util.concurrent.CompletableFuture;
 public class AsyncCheckout {
     public static void main(String[] args) {
         System.out.println("user clicked checkout...");
+
+        // 1. Start an asynchronous task (SupplyAsync)
         CompletableFuture<String> checkoutprocess = CompletableFuture.supplyAsync(() -> {
-            simulateDelay(2000);
+            simulateDelay(3000);
             return "Payment Successful";
         });
+
+        checkoutprocess.thenApply(status->status+" Receipt Printed");
+
+        // 2. Chain a "callback" - This happens automatically when the payment finishes
         checkoutprocess.thenAccept(result ->{
             System.out.println("status:"+ result);
             System.out.println("sending confirmation email");
         });
-        for(int i=0;i<6;i++){
+
+        // 3. The Main Thread is NOT blocked. It can do other things.
+        for(int i=0;i<10;i++){
             System.out.println("main running"+i);
-            simulateDelay(500);
+            simulateDelay(1000);
         }
+
+        // Keep the program alive long enough to see the async result
         checkoutprocess.join();
-        System.out.println("checkout complete");
+        System.out.println("checkout completed");
     }
 
     private static void simulateDelay(int ms){
